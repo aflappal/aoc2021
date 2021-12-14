e7d735ce43db4c03a3dfafe362d9182985706c76
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2021-12-14 03:38:53 +0200

    Day 6: Simplify

--- src/06.cpp
+++ src/06.cpp
@@ -8,24 +8,19 @@
 using std::cout, std::cerr;
 
 const auto N = 9;
+using inttype = unsigned long;
+using container = std::array<inttype, N>;
 
 class Counts {
 public:
-    Counts(const std::array<unsigned long, N>& initial)
+    Counts(const container& initial)
         : _start_idx{0}, _days_elapsed{0}, _counts{initial}
     {}
-    auto size() const { return _counts.size(); }
-    auto operator[](std::size_t i) const {
-        return _counts[(_start_idx + i) % size()];
-    }
-    auto& operator[](std::size_t i) {
-        return _counts[(_start_idx + i) % size()];
-    }
     void simulate() {
         // 0s spawn new 8s and reset to 6s. At this point the next 6s are still
         // 7s.
-        (*this)[7] += (*this)[0];
-        _start_idx = (_start_idx + 1) % size();
+        _counts[idx(7)] += _counts[idx(0)];
+        _start_idx = (_start_idx + 1) % _counts.size();
         ++_days_elapsed;
     }
     void simulate_until(int day) {
@@ -33,14 +28,17 @@ public:
             simulate();
         }
     }
-    unsigned long count() const {
-        return std::accumulate(_counts.begin(), _counts.end(), 0ul);
+    inttype count() const {
+        return std::accumulate(_counts.begin(), _counts.end(), inttype{0});
     }
 
 private:
+    std::size_t idx(std::size_t i) const {
+        return (_start_idx + i) % _counts.size();
+    }
     std::size_t _start_idx;
     int _days_elapsed;
-    std::array<unsigned long, N> _counts;
+    container _counts;
 };
 
 auto make_initial_counts(std::istream& is) {
@@ -50,7 +48,7 @@ auto make_initial_counts(std::istream& is) {
 
     std::istringstream iss{nums};
     int num;
-    std::array<unsigned long, N> arr = {0};
+    container arr = {0};
     while (iss >> num) {
         arr[num]++;
     }
@@ -60,15 +58,14 @@ auto make_initial_counts(std::istream& is) {
 
 auto ifs = au::get_ifstream("inputs/06.txt");
 const auto initial_counts = make_initial_counts(ifs);
+auto counts = Counts(initial_counts);
 
 void solve_a() {
-    auto counts = Counts(initial_counts);
     counts.simulate_until(80);
     cout << counts.count() << '\n';
 }
 
 void solve_b() {
-    auto counts = Counts(initial_counts);
     counts.simulate_until(256);
     cout << counts.count() << '\n';
 }
