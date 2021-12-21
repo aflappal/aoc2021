fd31fd4fe09d0549d2c87988f682dace4a009585
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2021-12-21 04:21:01 +0200

    Day 9: Separate coordinates and values
    Mainly to avoid having to set dummy values when just dealing with the
    windows; they need only coordinates.

--- src/09.cpp
+++ src/09.cpp
@@ -11,10 +11,14 @@ using std::cout, std::cerr;
 struct Point {
     int x;
     int y;
+};
+
+struct Cell {
+    Point coords;
     int val;
 };
 
-using grid_type = std::vector<std::vector<Point>>;
+using grid_type = std::vector<std::vector<Cell>>;
 using window_type = std::array<Point, 4>;
 
 auto parse() {
@@ -22,7 +26,7 @@ auto parse() {
     grid_type grid;
     std::string line;
     for (std::size_t y = 0; std::getline(ifs, line); ++y) {
-        std::vector<Point> line_nums;
+        std::vector<Cell> line_nums;
         for (std::size_t x = 0; x < line.size(); ++x) {
             line_nums.push_back({(int)x, (int)y, line[x]-'0'});
         }
@@ -33,12 +37,11 @@ auto parse() {
 
 const auto input = parse();
 
-// Window of dummy points; just using them for the coordinates
 auto basic_window(const Point& p) {
     return window_type {{
-                         {p.x, p.y-1, -1},
-        {p.x-1, p.y, -1},     /* p */     {p.x+1, p.y, -1},
-                         {p.x, p.y+1, -1}
+                     {p.x, p.y-1},
+        {p.x-1, p.y},   /* p */   {p.x+1, p.y},
+                     {p.x, p.y+1}
     }};
 }
 
@@ -56,14 +59,14 @@ auto elements(const grid_type& grid, auto window) {
 
 void solve_a() {
     auto grid = input;
-    auto is_low = [&](const Point& p) {
-        auto window = limit(basic_window(p), grid.back().back());
-        auto higher = [&](const Point& other) { return other.val > p.val; };
+    auto is_low = [&](const Cell& c) {
+        auto window = limit(basic_window(c.coords), grid.back().back().coords);
+        auto higher = [&](const Cell& other) { return other.val > c.val; };
         return std::ranges::all_of(elements(grid, window), higher);
     };
     auto low_points = grid | std::views::join | std::views::filter(is_low);
-    auto accu_func = [&](int accu, const Point& p) {
-        return accu + p.val + 1;
+    auto accu_func = [&](int accu, const Cell& c) {
+        return accu + c.val + 1;
     };
     auto sum = std::accumulate(low_points.begin(), low_points.end(), 0, accu_func);
     cout << sum << '\n';
