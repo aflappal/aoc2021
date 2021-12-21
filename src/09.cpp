ae288c0c6980451c23fdd0ed757ea0a746a32738
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2021-12-21 03:52:47 +0200

    Day 9: Fold val into Point

--- src/09.cpp
+++ src/09.cpp
@@ -8,57 +8,42 @@
 
 using std::cout, std::cerr;
 
-// XXX include val into point? Wouldn't work too well with the static windowing
 struct Point {
     int x;
     int y;
+    int val;
 };
 
-using grid_type = std::vector<std::vector<int>>;
+using grid_type = std::vector<std::vector<Point>>;
 using window_type = std::array<Point, 4>;
-
-auto at(const grid_type& grid, const Point& p) {
-    return grid[p.y][p.x];
-}
 
 auto parse() {
     auto ifs = au::get_ifstream("inputs/09.txt");
     grid_type grid;
     std::string line;
-    while (std::getline(ifs, line)) {
-        std::vector<int> line_nums;
-        for (auto c : line) {
-            line_nums.push_back(c - '0');
+    for (std::size_t y = 0; std::getline(ifs, line); ++y) {
+        std::vector<Point> line_nums;
+        for (std::size_t x = 0; x < line.size(); ++x) {
+            line_nums.push_back({(int)x, (int)y, line[x]-'0'});
         }
         grid.push_back(line_nums);
     }
     return grid;
 }
 
-auto make_points(const grid_type& grid) {
-    std::vector<Point> points;
-    for (std::size_t y = 0; y < grid.size(); ++y) {
-        auto row = grid[y];
-        for (std::size_t x = 0; x < row.size(); ++x) {
-            points.push_back(Point{(int)x, (int)y});
-        }
-    }
-    return points;
-}
+const auto input = parse();
 
-const auto input_vals = parse();
-const auto input_points = make_points(input_vals);
-
-auto neighbours(const Point& p) {
+// Window of dummy points; just using them for the coordinates
+auto basic_window(const Point& p) {
     return window_type {{
-                     {p.x, p.y-1},
-        {p.x-1, p.y},   /* p */   {p.x+1, p.y},
-                     {p.x, p.y+1}
+                         {p.x, p.y-1, -1},
+        {p.x-1, p.y, -1},     /* p */     {p.x+1, p.y, -1},
+                         {p.x, p.y+1, -1}
     }};
 }
 
 auto limit(window_type&& window, const Point& se_corner) {
-    auto within_grid = [&](Point& p) {
+    auto within_grid = [&](const Point& p) {
         return 0 <= p.x && p.x <= se_corner.x && 0 <= p.y && p.y <= se_corner.y;
     };
     return window | std::views::filter(within_grid);
@@ -66,20 +51,19 @@ auto limit(window_type&& window, const Point& se_corner) {
 
 auto elements(const grid_type& grid, auto window) {
     return window
-        | std::views::transform([&](Point& p) { return at(grid, p); });
+        | std::views::transform([&](const Point& p) { return grid[p.y][p.x]; });
 }
 
 void solve_a() {
-    auto grid = input_vals;
-    auto points = input_points;
+    auto grid = input;
     auto is_low = [&](const Point& p) {
-        auto window = limit(neighbours(p), points.back());
-        auto higher = [&](int val) { return val > at(grid, p); };
+        auto window = limit(basic_window(p), grid.back().back());
+        auto higher = [&](const Point& other) { return other.val > p.val; };
         return std::ranges::all_of(elements(grid, window), higher);
     };
-    auto low_points = points | std::views::filter(is_low);
+    auto low_points = grid | std::views::join | std::views::filter(is_low);
     auto accu_func = [&](int accu, const Point& p) {
-        return accu + at(grid, p) + 1;
+        return accu + p.val + 1;
     };
     auto sum = std::accumulate(low_points.begin(), low_points.end(), 0, accu_func);
     cout << sum << '\n';
