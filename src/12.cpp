5116c6c7150ba19419d5619f747d8ff1c1ad5bf6
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2021-12-31 01:50:52 +0200

    Day 12: Use memoization
    Provides ~10x speed-up; takes ~7 ms to run now.

--- src/12.cpp
+++ src/12.cpp
@@ -7,15 +7,17 @@
 #include <ranges>
 #include <cctype>
 #include <algorithm>
+#include <utility>
 #include "util.h"
 
 using std::cout, std::cerr;
 
-using graph_type = std::map<std::string, std::vector<std::string>>;
+using Graph = std::map<std::string, std::vector<std::string>>;
+using Cache = std::map<std::tuple<std::string, std::set<std::string>, bool>, int>;
 
 auto parse() {
     auto ifs = au::get_ifstream("inputs/12.txt");
-    graph_type graph;
+    Graph graph;
     std::string line;
     while (std::getline(ifs, line)) {
         std::istringstream iss{line};
@@ -31,17 +33,22 @@ auto parse() {
 const auto input = parse();
 
 auto is_small(const std::string& node) {
-    //const auto is_lower = [](unsigned char c) { return std::islower(c); };
-    //return std::ranges::all_of(node, is_lower);
     // With the given input it's enough to check the first character
     return std::islower(node[0]);
 }
 
 // XXX graph can't be const for some reason
-int paths_to_end(graph_type& graph, const std::string& node,
-                 std::set<std::string>& visited, bool visited_twice) {
+int paths_to_end(Graph& graph, const std::string& node,
+                 std::set<std::string>& visited, Cache& cache,
+                 bool visited_twice) {
     if (node == "end")
         return 1;
+
+    const auto cache_key = std::make_tuple(node, visited, visited_twice);
+    auto cache_it = cache.find(cache_key);
+    if (cache_it != cache.end()) {
+        return cache_it->second;
+    }
 
     bool visited_this_twice = false;
     if (is_small(node)) {
@@ -57,24 +64,28 @@ int paths_to_end(graph_type& graph, const std::string& node,
         return n != "start" && (!visited_twice || !visited.contains(n));
     };
     for (const auto& neigh : graph[node] | std::views::filter(can_visit)) {
-        paths += paths_to_end(graph, neigh, visited, visited_twice);
+        paths += paths_to_end(graph, neigh, visited, cache, visited_twice);
     }
+
     if (!visited_this_twice)
         visited.erase(node);
+    cache[cache_key] = paths;
     return paths;
 }
 
 void solve_a() {
     auto graph = input;
     auto visited = std::set<std::string>();
+    Cache cache;
     // visited_twice = true from the start to visit small nodes only once
-    cout << paths_to_end(graph, "start", visited, true) << '\n';
+    cout << paths_to_end(graph, "start", visited, cache, true) << '\n';
 }
 
 void solve_b() {
     auto graph = input;
     auto visited = std::set<std::string>();
-    cout << paths_to_end(graph, "start", visited, false) << '\n';
+    Cache cache;
+    cout << paths_to_end(graph, "start", visited, cache, false) << '\n';
 }
 
 int main() {
