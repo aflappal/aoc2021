00ec690e03e1de67b7725f9b6b6193e6c3555b77
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2022-01-02 14:12:21 +0200

    Day 14: Solve b

--- src/14.cpp
+++ src/14.cpp
@@ -38,30 +38,53 @@ auto parse() {
 
 const auto input = parse();
 
-auto augment(const std::string& current, const Rules& rules) {
-    std::string next = current.substr(0, 1);
-    for (std::size_t i = 1; i < current.size(); ++i) {
-        auto pair = current.substr(i-1, 2);
-        next += rules.at(pair) + current[i];
+auto augment(const std::map<std::string, long> current, const Rules& rules) {
+    std::map<std::string, long> next;
+    for (const auto& [pair, n] : current) {
+        const auto product = rules.at(pair);
+        const std::string first = pair[0] + product;
+        const std::string second = product + pair[1];
+        next[first] += n;
+        next[second] += n;
     }
     return next;
 }
 
-void solve_a() {
+auto make_pairs(const std::string& s) {
+    std::map<std::string, long> pairs;
+    for (std::size_t i = 1; i < s.size(); ++i) {
+        auto pair = s.substr(i-1, 2);
+        ++pairs[pair];
+    }
+    return pairs;
+}
+
+auto answer_after_iters(int iters) {
     const auto& [initial, rules] = input;
-    auto current = initial;
-    for (auto i = 0; i < 10; ++i) {
-        current = augment(current, rules);
+    auto pairs = make_pairs(initial);
+    for (auto i = 0; i < iters; ++i) {
+        pairs = augment(pairs, rules);
     }
 
-    auto occurrences = std::map<char, int>();
-    for (const auto& c : current)
-        ++occurrences[c];
+    auto occurrences = std::map<char, long>();
+    for (const auto& [pair, n] : pairs) {
+        occurrences[pair[1]] += n;
+    }
+    // Before only the second char of each pair was added because the first is
+    // the second of some other pair. Except for the first character; add it
+    // explicitly here.
+    // XXX is this correct..?
+    ++occurrences[initial[0]];
     auto [min, max] = std::ranges::minmax(occurrences | std::views::values);
-    cout << max - min << '\n';
+    return max-min;
+}
+
+void solve_a() {
+    cout << answer_after_iters(10) << std::endl;
 }
 
 void solve_b() {
+    cout << answer_after_iters(40) << std::endl;
 }
 
 int main() {
